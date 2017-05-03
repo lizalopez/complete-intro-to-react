87a36eda06fe6a5eec9b79f33e126878900e71cd
LizaLopez <liza@doc.ai>
2017-05-03 09:15:17 -0700

    Make a second component by building it off of another small component

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,16 +1,32 @@
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
+//create the titulo stamp here
+var titulo = React.createClass({
+	render: function() {
+		return div(null,
+			h1(null, 'Mira este component')
+		)
+	}
+})
+
+//in this other class, you created multiple instances of the titulo component
+//so you build small compoments, then you build bigger components that encapsulate small components --> that's what react is
 var primerComponent = React.createClass({
 	render: function() {
 		return (
-			div(null,
-				h1(null,'el Primer component!')
+				//can give multiple seperate parameters, or an array
+			div(null, 
+				// [
+				React.createElement(titulo),
+				React.createElement(titulo),
+				React.createElement(titulo),
+				React.createElement(titulo)
+				// ]
 			)
 		)
 	}
 })
 
-console.log(ReactDOM);
 ReactDOM.render(React.createElement(primerComponent), document.
 getElementById('app'))
