3bdb76de12b5ef1e0c29efa2f25da0b907edbfeb
LizaLopez <liza@doc.ai>
2017-11-08 15:52:22 -0800

    Adds jsx to clientapp file

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -5,34 +5,23 @@ import React from 'react'
 // var ReactDOM = require('react-dom');
 
 import ReactDOM from 'react-dom'
-import titulo from './miTitulo'
-// var titulo = require('./miTitulo')
-var div = React.DOM.div;
-
-// makes a factory method for other components, so here it makes a factory method (that will run the helper functions?)
-var miFactoriaDeTitulo = React.createFactory(titulo)
+import MiTitulo from './miTitulo'
 
 // in this other class, you created multiple instances of the titulo component
 // so you build small compoments, then you build bigger components that encapsulate small components --> that's what react is
 var primerComponent = React.createClass({
   render: function () {
     return (
-          // can give multiple seperate parameters, or an array
-      div(null,
-        // [
-        // change the attributes
-        miFactoriaDeTitulo({titulo: 'I ', color: 'papayawhip'}),
-        // pass the object here (titulo) from parent (primerComponente) to child (titulo).
-        // child can read the attributes from it's parent (in this case, color and titulo), but not modify them. READ ONLY
-        // we are creating method, so in this case it's .titulo, but it could be anything
-        miFactoriaDeTitulo({titulo: 'don\'t want no scrub', color: 'burlywood'}),
-        miFactoriaDeTitulo({titulo: 'want no scrub', color: 'navahowhite'}),
-        miFactoriaDeTitulo({titulo: 'no scrub', color: 'olivedrab'})
-        // don't have to only pass down string, can pass fx's, properties -- anything that can be passed by reference in JS
-
-        // ]
-      )
+      <div>
+        <MiTitulo titulo="Heyyyy there props" color='teal' />
+        <MiTitulo titulo="props are looking good" color='blanchedalmond' />
+        <MiTitulo titulo="real good" color='navajowhite' />
+      </div>
     )
+    // can insert miTiulo component like it's a real HTML element, give it properties like you woudl a normal html element, made self closing tag since there are no children. 
+    // components must be uppercased, to signify it's your component you created, otherwise it uputs literally unto dom (web component)
+    // there's no black magic. like if you want to siblings (div) at top level components, it'd be like return x, y, 
+    // so can only return one topmost component. so yu can wrap anything in div
   }
 })
 
--- js/miTitulo.js
+++ js/miTitulo.js
@@ -2,12 +2,8 @@ import React from 'react'
 // var React = require('react');
 //signaling to packager to grab react module (from node module dir) as React variable, now react var doens't come from global scope, it's locally injected
 
-var div = React.DOM.div
-var h1 = React.DOM.h1
-var h4 = React.DOM.h4
-
 // create the titulo stamp here
-var titulo = React.createClass({
+var MiTitulo = React.createClass({
   render: function () {
     const style = {color: this.props.color}
     return (
@@ -17,16 +13,18 @@ var titulo = React.createClass({
         </h1>
       </div>
     )
-  }
-})
       // insdie h1 -- signify that you want it to output whatever the value is. if we put out this.props.title without
       // any object marks outside of it, it would just ouptut a string. Also, you could put this.props.title.toUpperCase(),
       // brackes indicate that we are inserting a JS expression
+      // this gets transpiled into React.createElement('div', null, and children of element)  -- can read how it's transpiled in the bundle.js source map file
+
+  }
+})
 // this mashes together all markup and behavior, which makes a very powerful debugging tool 
 
-export default titulo
+export default MiTitulo
 // es6 module. most common way is commonjs -- this is syntax of future
 // signals that anytime anyone requests this module titulo, it will hand over this code bc this is what we're exporting
-//default means you will export multiple dffeent things
+//default means you will export multiple dfferent things
 
 
